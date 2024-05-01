96cc3d0e1e211cdec83ccea140697c1bf55466b8
anthonyero <anthonyero@gmail.com>
2024-04-30 22:32:04 -0500

    Defined a putDb function to update the content of the sole object containing text editor content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ id: 1, content }); // We only expect one instance of the text content, so I assigned it to a value of `id`: 1 to continually update
+    const result = await request;
+    return result;
+  } catch (err) {
+    console.error('putDb not implemented');
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
